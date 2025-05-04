fe423dd955748e0ef91501b5e53e8ad195668442
lmoheyma <louismoheymani@gmail.com>
2025-05-04 18:17:00 +0200

    ✅ : Ex05: Convert RPN to NNF + gitignore

--- ex00/add.py
+++ ex00/add.py
@@ -1,29 +1,31 @@
 def adder(a: int, b: int) -> int:
-	if a < 0 or b < 0 or not isinstance(a, int) or not isinstance(b, int):
-		print("Only naturel numbers!")
-		return
-	keep = (a & b) << 1
-	res = a ^ b
-	if keep == 0:
-		return res
-	return adder(keep, res)
+    if a < 0 or b < 0 or not isinstance(a, int) or not isinstance(b, int):
+        print("Only naturel numbers!")
+        return
+    keep = (a & b) << 1
+    res = a ^ b
+    if keep == 0:
+        return res
+    return adder(keep, res)
+
 
 def main():
-	x = 0
-	y = 0
-	print(f"{x} + {y} = {adder(x, y)}")
+    x = 0
+    y = 0
+    print(f"{x} + {y} = {adder(x, y)}")
 
-	x = 5
-	y = 7 
-	print(f"{x} + {y} = {adder(x, y)}")
-	
-	x = 101
-	y = 101
-	print(f"{x} + {y} = {adder(x, y)}")
+    x = 5
+    y = 7
+    print(f"{x} + {y} = {adder(x, y)}")
 
-	x = -5
-	y = 7 
-	print(f"{x} + {y} = {adder(x, y)}")
+    x = 101
+    y = 101
+    print(f"{x} + {y} = {adder(x, y)}")
+
+    x = -5
+    y = 7
+    print(f"{x} + {y} = {adder(x, y)}")
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- ex05/NegationNormalForm.py
+++ ex05/NegationNormalForm.py
@@ -1,39 +1,10 @@
 import sys
-
-sys.path.append("../")
+sys.path.append('../')
 from cls.ASTNode import ASTNode
 
-class NegationNormalForm:
-	def __init__(self) -> None:
-		self.NNF = ""
 
-	def formulaToAST(self, formula: str) -> str:
-		AST = ASTNode(None)
-		rootNode = AST.tree_generation(list(formula))
-		# if rootNode.value == "!":
-		# 	for element in formula:
-		# 		if element == "&":
-		# 			NNF += "|"
-		# 		elif element == "|":
-		# 			NNF += "&"
-		# 		else:
-		# 			NNF += element + "!"	
-		AST.print_tree(rootNode, 0)
-		# rootNode = rootNode.left
-		# AST.parsing(rootNode)
-		# AST.print_tree(rootNode, 0)
-		self.ASTToNNF(rootNode)
-		print()
-
-	def ASTToNNF(self, node):
-		if node is not None:
-			self.printNNF(node.right)
-			self.NNF += node.value
-			self.printNNF(node.left)
-
-
-def main():
-	def preEvaluating(formula: str):
+def RPNtoNNF(formula):
+	def preprocessing(formula: str):
 		special_cases = {
 			'AB>': 'A!B|',
 			'AB=': 'AB&A!B!&|'
@@ -42,17 +13,29 @@ def main():
 			if case in formula:
 				startIndex = formula.find(case)
 				formula = formula[:startIndex] + special_cases[case] + formula[startIndex + 3:]
-	# NNF = NegationNormalForm()
-	# NNF.formulaToAST("AB&!")
-	formula = 'AB&!'
+		return formula
+	symbols = {
+		'&': '|',
+		'|': '&'
+	}
 	res = ''
-	preEvaluating(formula)
-	if formula[-1]:
+	formula = preprocessing(formula)
+	if formula[-1] == '!':
 		for element in formula:
 			if element.isalpha():
-				res.append(element + '!')
+				res += element + '!'
+			if element in symbols:
+				res += symbols[element]
+	return res or formula
 
-	# NegationNormalForm("A!B!&C!|")
 
-if __name__ == "__main__":
+def main():
+	print(RPNtoNNF('AB&!'))
+	print(RPNtoNNF('AB|!'))
+	print(RPNtoNNF('AB>'))
+	print(RPNtoNNF('AB='))
+	print(RPNtoNNF('AB|C&!'))
+	
+
+if __name__ == '__main__':
 	main()
